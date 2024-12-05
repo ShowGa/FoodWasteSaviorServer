2fab8ad22575eabbd9c4efbd703c0aebe92ef3b0
ShowGa <showgacareer@gmail.com>
2024-12-05 09:34:39 +0800

    <Initialization> Initialize the project

---
+++ src/main/java/com/foodwastesavior/webapp/FoodwastesaviorServerApplication.java
@@ -0,0 +1,13 @@
+package com.foodwastesavior.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodwastesaviorServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodwastesaviorServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foodwastesavior/webapp/FoodwastesaviorServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodwastesavior.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodwastesaviorServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
